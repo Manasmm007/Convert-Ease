ced1f9a4bd02899633386d9eadabb4a63ea523d4
Manasmm007 <91602226+Manasmm007@users.noreply.github.com>
2023-11-01 13:14:55 +0530

    Create function_calculate_BMI.js

---
+++ function_calculate_BMI.js
@@ -0,0 +1,17 @@
+function calBMI(){
+      var height = parseFloat(document.getElementById("height").value);
+      var mass = document.getElementById("mass").value;
+
+      var result4 = mass/(height*height);
+      var a="";
+
+      if(result4<18.5)
+              a="Underweight"
+      else if(result4<=25)
+              a="Normal Weight"
+      else if(result4<30)
+              a="Overweight"
+      else
+              a="Obese"
+      document.getElementById("result4").innerHTML = "Result: " + result4.toFixed(2) + " " + a;
+}
